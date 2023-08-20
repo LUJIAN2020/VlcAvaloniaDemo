44035f51cecec9c5178c490d5f3aef6c12e9bc06
ImLuJian <504439788@qq.com>
2023-08-20 17:01:54 +0800

    添加项目文件。

---
+++ VlcAvaloniaDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using LibVLCSharp.Shared;
+
+namespace VlcAvaloniaDemo.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        //Linux使用方法
+        //Ubuntu下运行一下这两个命令安装包
+        //sudo apt install libvlc-dev
+        //sudo apt install vlc
+
+        private readonly LibVLC _libVlc = new LibVLC();
+        public MainWindowViewModel()
+        {
+            MyMediaPlayer = new MediaPlayer(_libVlc);
+        }
+
+        private MediaPlayer? myMediaPlayer;
+        public MediaPlayer? MyMediaPlayer
+        {
+            get { return myMediaPlayer; }
+            set { SetProperty(ref myMediaPlayer, value); }
+        }
+
+
+        [RelayCommand]
+        public void OptionsCommand(string content)
+        {
+            switch (content)
+            {
+                case "播放":
+                    {
+                        if (MyMediaPlayer?.State == VLCState.Paused)
+                        {
+                            MyMediaPlayer?.Play();
+                        }
+                        else
+                        {
+                            using var media = new Media(_libVlc, "Avalonia XPF.mp4");
+                            MyMediaPlayer?.Play(media);
+                        }
+                    }
+                    break;
+                case "停止":
+                    MyMediaPlayer?.Stop();
+                    break;
+                case "暂停":
+                    MyMediaPlayer?.Pause();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ VlcAvaloniaDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VlcAvaloniaDemo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
